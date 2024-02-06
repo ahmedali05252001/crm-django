61396bbdba68b8d446bf23b3a33d3d4bc1d79cc4
Ahmed Ali <ahmed.ali05252001@gmail.com>
2024-02-06 16:12:08 +0300

    Refactoring the project.Convert the function based views of the leads to class base views.Creating the namespaces for each of the apps

--- crmProject/dashboard/views.py
+++ crmProject/dashboard/views.py
@@ -12,6 +12,7 @@ def dashboard(request):
     team = Team.objects.filter(created_by = request.user)[0]
     leads = Lead.objects.filter(team = team, converted_to_client = False).order_by("-created_at")[0:5]
     clients = Client.objects.filter(team = team).order_by("-created_at")[0:5]
+    
     return render(request, "dashboard/dashboard.html",{
         "leads" : leads,
         "clients" : clients,
